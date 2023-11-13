ab8248af09aeb8df070102edc4c01dad51434fcd
DreamySkrell <>
2023-11-13 22:12:14 +0100

    mapmanip stuff

--- dmsrc/mapmanip.dm
+++ dmsrc/mapmanip.dm
@@ -1,1 +1,1 @@
-#define rustg_mapmanip_load_map(map_fname, config_fname) RUSTG_CALL(RUST_G, "mapmanip_load_map")(map_fname, config_fname)
+#define rustg_mapmanip_load_map(map_fname) RUSTG_CALL(RUST_G, "mapmanip_load_map")(map_fname)
--- src/mapmanip.rs
+++ src/mapmanip.rs
@@ -1,11 +1,14 @@
 use crate::error::Result;
+use rand::seq::SliceRandom;
 
-byond_fn!(fn mapmanip_load_map(path_dmm, path_config) {
-    load_map_guard(path_dmm, path_config).ok()
+byond_fn!(fn mapmanip_load_map(path_dmm) {
+    load_map_guard(path_dmm).ok()
 });
 
-fn load_map_guard(path_dmm: &str, path_config: &str) -> Result<String> {
-    let loaded = load_map(path_dmm, path_config);
+fn load_map_guard(path_dmm: &str) -> Result<String> {
+    let path_config = path_dmm.replace(".dmm", ".toml");
+
+    let loaded = load_map(path_dmm, &path_config);
     return if loaded.is_err() {
         crate::file::read(path_dmm)
     } else {
@@ -14,7 +17,26 @@ fn load_map_guard(path_dmm: &str, path_config: &str) -> Result<String> {
 }
 
 fn load_map(path_dmm: &str, path_config: &str) -> Result<String> {
-    let dmm = crate::file::read(path_dmm)?;
-    let _json = crate::file::read(path_config)?;
-    Ok(dmm)
+    let dmm_raw = crate::file::read(path_dmm)?;
+    let _config_raw = crate::file::read(path_config)?;
+
+    let dmm_parsed = dmm_parser_rs::dmmr::parse(&dmm_raw);
+
+    let mut dmm_unpacked = dmm_parser_rs::dmmr::unpack(&dmm_parsed);
+
+    let src_coord = [(60, 40), (65, 40), (70, 40), (60, 35), (65, 35), (70, 35)]
+        .choose(&mut rand::thread_rng())
+        .unwrap()
+        .clone();
+    let src_coord = (src_coord.0 - 1, src_coord.1 - 1);
+    let dst_coord = (65 - 1, 45 - 1);
+    let size = (4, 4);
+
+    let extracted =
+        dmm_parser_rs::copypaste::extract(&dmm_unpacked, (src_coord).into(), size.into());
+    dmm_parser_rs::copypaste::insert(&extracted, &mut dmm_unpacked, dst_coord.into());
+
+    let dmm_parsed = dmm_parser_rs::dmmr::pack(&dmm_unpacked);
+
+    Ok(dmm_parser_rs::dmmr::print(&dmm_parsed))
 }
